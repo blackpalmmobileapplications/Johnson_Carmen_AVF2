f4c1798f5e2f838baf620cacc3d74cbebac13762
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-11-05 22:12:49 -0800

    added files to ios and android

---
+++ PG/gen/cj/pg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cj.pg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PG/src/cj/pg/PG.java
@@ -0,0 +1,13 @@
+package cj.pg;
+
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+public class PG extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
